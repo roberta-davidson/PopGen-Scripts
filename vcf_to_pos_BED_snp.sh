2295ca658cb40244e23a03e56e7ed15136ecf303
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2022-03-22 17:49:35 +1030

    Update vcf_to_pos_BED_snp.sh

--- vcf_to_pos_BED_snp.sh
+++ vcf_to_pos_BED_snp.sh
@@ -15,19 +15,22 @@ VCF=$1
 
 #count header rows
 L="$(grep "#" ${VCF}.vcf | wc -l)"
-echo ${L}
+echo "Header rows - ${L}"
 
 #remove vcf headers
 awk 'NR>l {print $0}' l="${L}" ${VCF}.vcf > ${VCF}.nohead.vcf
 
-#To create the .pos you do:
-awk -v OFS='\t' '{print $1,$2}' ${VCF}.nohead.vcf > ${VCF}.pos
-
-#To create the .snp you do:
+#To create the .snp of only SNPs, remove longer variants and rename snpID to "CHR_SITE", you do:
 awk -v OFS='\t' 'length($5)<2 && length($4)<2 {print $1"_"$2,$1,0,$2,$4,$5}' ${VCF}.nohead.vcf > ${VCF}.snp
 
 #Create file correlating snpID with CHR_POS for safekeeping
 awk -v OFS='\t' '{print $1"_"$2,$3}'  ${VCF}.nohead.vcf > ${VCF}.snp_names
 
-#Create .BED file
-awk -v OFS='\t' '{n=1;print $1,$2-n,$2}'  ${VCF}.nohead.vcf > ${VCF}.BED
+#To create the .pos from snp sites you do:
+awk -v OFS='\t' '{print $3,$4}' ${VCF}.snp > ${VCF}.pos
+
+#Create .BED from snp sites file
+awk -v OFS='\t' '{n=1;print $3,$4-n,$4}' ${VCF}.snp > ${VCF}.BED
+
+# Clean up
+rm ${VCF}.nohead.vcf
