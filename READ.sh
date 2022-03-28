d43fa13eb1c78825ca01e53b863e827eb6ff3b61
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2022-03-28 11:26:35 +1030

    Update READ.sh

--- READ.sh
+++ READ.sh
@@ -7,16 +7,21 @@ ml modulefiles/arch/haswell
 ml plink/1.90beta-4.4-21-May
 conda activate read
 
-#requires python 2.7
+#requirements 
+#python 2.7
+#R
+
 
 # READ input: PLINK .tped and .tfam files
 # prodece tped by --recode transpose
 
-${DATA}=${1}
+DATA=$1
 
 #Prepare dataset
 
 plink --bfile ${DATA} --keep-allele-order --maf 0.01 --geno 0.999999 --mind 1.0 --allow-no-sex --recode transpose --out ${DATA}
+
+rm ${DATA}.nosex
 
 # Run READ
 python /hpcfs/users/a1717363/Programs/read/READ.py ${DATA}
