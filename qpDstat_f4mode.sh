13c0b31b5148420933130df8a7adc8540494c1cf
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2022-08-29 15:33:25 -0700

    Update qpDstat_f4mode.sh

--- qpDstat_f4mode.sh
+++ qpDstat_f4mode.sh
@@ -6,9 +6,66 @@ ml OpenBLAS/0.3.1
 
 #assign variable to dataset called in command
 in1=$1
+in2=$2
 
-#write pops file of every pop in dataset
-awk '{print $3}' ${in1}.ind | sort | uniq > ${in1}.poplist.txt
+# Automatically merge target dataset with reference fileset
+
+mergeit -p <(echo "geno1: ${in1}.geno
+snp1:  ${in1}.snp
+ind1:  ${in1}.ind
+geno2: ${in2}.geno
+snp2:  ${in2}.snp
+ind2:  ${in2}.ind
+genooutfilename:   ${in1}_ref.geno
+snpoutfilename:    ${in1}_ref.snp
+indoutfilename:    ${in1}_ref.ind
+outputformat:  EIGENSTRAT
+docheck: YES
+strandcheck:  YES
+hashcheck: NO")
+
+
+# Automatically merge with Mbuti outgroup fileset
+
+mergeit -p <(echo "geno1: ${in1}_ref.geno
+snp1:  ${in1}_ref.snp
+ind1:  ${in1}_ref.ind
+geno2: Mbuti.geno
+snp2:  Mbuti.snp
+ind2:  Mbuti.ind
+genooutfilename:   ${in1}_ref_Mbuti.geno
+snpoutfilename:    ${in1}_ref_Mbuti.snp
+indoutfilename:    ${in1}_ref_Mbuti.ind
+outputformat:  EIGENSTRAT
+docheck: YES
+strandcheck:  YES
+hashcheck: NO")
+
+#auto list all pops in input data for poplist file
+awk '{print $3}' ${in1}_ref_Mbuti.ind | sort | uniq > ${in1}.poplist.txt
+
+#write popfile *.trees.txt to specify quadruples to be tested
+#of the form: Mbuti SAgroup Chono1 Chono2
+
+echo "writing popfile with trees to test"
+targetpops=$(awk '{print $3}' ${in1}.ind | sort | uniq)
+refpops=$(awk '{print $3}' ${in2}.ind | sort | uniq)
+
+rm ${in1}.trees.txt
+
+for X in ${refpops}; do
+  for Y in ${targetpops}; do
+    for Z in ${targetpops}; do
+        echo "Mbuti ${X} ${Y} ${Z} Mbuti" >> ${in1}.trees.txt
+    done
+  done
+done
+
+# for slurm
+cat ${in1}.trees.txt
+
+echo "done
+Running f4 tests"
 
 #Run on every quadruple combination
 /hpcfs/users/a1717363/Programs/AdmixTools/bin/qpDstat \
@@ -16,6 +73,7 @@ awk '{print $3}' ${in1}.ind | sort | uniq > ${in1}.poplist.txt
 snpname:      ${in1}.snp
 genotypename: ${in1}.geno
 poplistname: ${in1}.poplist.txt
+popfilename: ${in1}.trees.txt
 f4mode:   YES") \
 > ${in1}.qpDstat_f4.out
 
